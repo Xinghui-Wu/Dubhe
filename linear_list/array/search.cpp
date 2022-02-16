6606792467a5e74cc46ae62654f10caab7c3885c
wuxinghui <xjtu.wuxinghui@gmail.com>
2022-02-17 00:37:52 +0800

    Implement binary search for array.

---
+++ linear_list/array/search.cpp
@@ -0,0 +1,32 @@
+#include "search.h"
+
+
+/**
+ * Find the element with the target value in a sorted array using binary search.
+ */
+int binary_search(std::vector<int>& nums, int target)
+{
+    int left = 0;
+	int right = nums.size() - 1;
+    int mid;
+
+    while (left <= right)
+    {
+		mid = (left + right) / 2;
+
+		if (nums[mid] < target)
+        {
+            left = mid + 1;
+        }
+		else if (nums[mid] > target)
+        {
+			right = mid - 1;
+        }
+		else
+        {
+			return mid;
+        }
+	}
+
+    return -1;
+}
---
+++ linear_list/array/search.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <vector>
+
+/**
+ * Find the element with the target value in a sorted array using binary search.
+ */
+int binary_search(std::vector<int>& nums, int target);
+
+#endif
